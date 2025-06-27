2e567f113b3a744f4a6bd901d822881cb1725933
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-27 23:36:25 +0800

    vue3

---
+++ git-goldshoot0720.github.io.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "vue3"
+git checkout -b master
+git remote add origin https://github.com/goldshoot0720/goldshoot0720.github.io.git
+git push -u origin master --force
+pause
---
+++ open-goldshoot0720.github.io-vue3.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\chbon\Documents\goldshoot0720.github.io\vue3"
+explorer http://localhost:3000
+npx serve
+pause
